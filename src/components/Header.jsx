dfbce291709e3b80cc68325a57984ba5ac5e528e
mhameenvp <mhdameenvp1126@gmail.com>
2024-02-16 04:53:27 -0800

    headers set

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">Ahammeds </span>
+          <span className="text-slate-500"> Estates</span>
+        </h1>
+        <form className="bg-slate-100 p-3 rounded-xl flex items-center">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="black" />
+        </form>
+
+        <ul className="flex gap-4">
+          <Link to='/home'>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+
+          <Link to='/About'>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to='/Sign-in'>
+            
+            <li className=" text-slate-700 hover:underline">Sign in </li>
+          </Link>
+
+         
+        </ul>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function sign() {
+  return (
+    <div>
+      <h1>this is Signin</h1>
+    </div>
+  )
+}
